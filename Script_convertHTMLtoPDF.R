9678754ee670000f45314324a2f360f295f47edd
Alyce <alycerussell88@gmail.com>
2020-05-21 14:40:40 +0800

    Delete Script_convertHTMLtoPDF.R

--- Script_convertHTMLtoPDF.R
+++
@@ -1,4 +0,0 @@
-#Code to format HTML to PDF file
-
-#just pass the HTML output file path to chrome_print()
-pagedown::chrome_print("C:/Users/aruss/Box Sync/R Workshops/ReproducibleResearch/ReproducibleResearchSeminar.html")
